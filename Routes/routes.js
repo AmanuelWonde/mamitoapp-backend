a86a595c29ec32c28fb1fceb2e5f2162e2f57d03
AbelMaireg <abelrighthere@gmail.com>
2023-11-08 14:40:32 +0300

    server setup

---
+++ Routes/routes.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const router1 = express.Router();
+
--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
 const multer = require('multer');
-const debug = require('debug');
 
 const app = express();
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    const debug = require('debug')('basic:server');
+    debug(`listening on port ${port}`)
+});
