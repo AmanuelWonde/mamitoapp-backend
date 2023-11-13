e8c39fd88b4ee362c1ff54f09e68f042390cc61a
AbelMaireg <abelrighthere@gmail.com>
2023-11-13 20:42:36 +0300

    -a

---
+++ Middleware/auth.js
@@ -0,0 +1,37 @@
+const JWT = require('jsonwebtoken');
+const { status, responseInstance } = require('../Models/response')
+
+module.exports = (req, res, next) => {
+    const debugg = require('debug')('auth');
+    let p1 = new Promise((resolve, reject) => {
+        const debug = require('debug')('auth:user');
+        const token = req.header('auth-token');
+        if (token) {
+            resolve(token);
+        } else {
+            debug('token is needed for authentication');
+            reject(new responseInstance(new status(5001, 'token is required'), 'use a token inorder to get services'))
+        }
+    });
+
+    const tokenAuthentication = (token) => {
+        return new Promise((resolve, reject) => {
+            try {
+                const verification = JWT.verify(token, process.env.jwtPrivateKey);
+                resolve(verification);
+            } catch (error) {
+                debug(error);
+                reject(new responseInstance(new status(5002, 'verification failed'), 'use a valid toke in order to get services'))
+            }
+        })
+    }
+
+    const verify = (verification) => {
+        next();
+    }
+
+    p1
+        .then((token) => tokenAuthentication(token))
+        .then((verification) => verify(verification))
+        .catch((error) => { debugg(error); res.send(error) });
+}
