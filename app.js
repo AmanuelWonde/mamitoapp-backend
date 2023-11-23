c0564329d1a3a125cbb6f1dbcc75a4b1fbbc77c1
Amanuel Wonde <134140189+amanuelWonde@users.noreply.github.com>
2023-11-23 08:42:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-const express = require("express");
-const app = express();
-const port = 4000;
 
-app.use(express.json());
-app.use("/questions", require("./Routes/questionRoutes"));
-app.use("/answers", require("./Routes/answerRoutes"));
-
-app.listen(port, () => {
-  const debug = require("debug")("basic:server");
-  debug(`listening on port ${port}`);
-  console.log("server is running");
-});
