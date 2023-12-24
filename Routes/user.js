cfcc5b5fa97e92b766e9efaa584173def4f5096a
Amanuel Wonde <amanuelwt@gmail.com>
2023-12-24 14:26:14 +0300

    add profile image routes

--- Routes/user.js
+++ Routes/user.js
@@ -14,18 +14,24 @@ router.post("/login", require("../Middleware/user/login"));
 
 router.put("/update", require("../Middleware/user/update"));
 
-router.post("/rcvfill", require("../Middleware/user/passwordRecoveryQuestionsFill"));
+router.post(
+  "/rcvfill",
+  require("../Middleware/user/passwordRecoveryQuestionsFill")
+);
 
-router.put('/changepassword', require("../Middleware/user/changepassword"));
+router.put("/changepassword", require("../Middleware/user/changepassword"));
 
-router.get('/getrcvqst', require('../Middleware/user/getRcvQst'));
+router.get("/getrcvqst", require("../Middleware/user/getRcvQst"));
 
 router.put("/forgotpassword", require("../Middleware/user/forgotpassword"));
 
+router.post(
+  "/profileimageupload",
+  upload("profileImages").single("profileImage"),
+  addProfileImage
+);
 
-
-
-
-// router.post("/profileimageupload", (req, res) => { });
+router.post("/deleteprofileimage", deleteProfileImage);
+router.get("/viewprofileimages/:userName", viewProfileImage);
 
 module.exports = { router };
