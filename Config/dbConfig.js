597f9f53fd40f49a28f528d1edd3098cfbb05322
amanuel wonde <amanuelwt@gmail.com>
2024-03-15 23:59:58 +0300

    change databes credentials

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -1,24 +1,9 @@
 const mysql = require("mysql2/promise");
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "amanuelwt",
-  password: "*rgz1BT)WrV4cXs*",
-  database: "mamito_app",
-  waitForConnections: true,
-  connectionLimit: 0,
-  maxIdle: 10,
-  idleTimeout: 60000,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
-});
-
-// server db configeration
 // const pool = mysql.createPool({
-//   host: "mamitoapp.com",
-//   user: "mamitogw_amanuelwonde",
-//   password: "X_Z)qu6;o$w6",
-//   database: "mamitogw_mamito",
+//   host: "localhost",
+//   user: "amanuelwt",
+//   password: "*rgz1BT)WrV4cXs*",
+//   database: "mamito_app",
 //   waitForConnections: true,
 //   connectionLimit: 0,
 //   maxIdle: 10,
@@ -27,4 +12,19 @@ const pool = mysql.createPool({
 //   enableKeepAlive: true,
 //   keepAliveInitialDelay: 0,
 // });
+
+// server db configeration
+const pool = mysql.createPool({
+  host: "mamitoapp.com",
+  user: "mamitogw_amanuelwonde",
+  password: "X_Z)qu6;o$w6",
+  database: "mamitogw_mamito",
+  waitForConnections: true,
+  connectionLimit: 0,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
 module.exports = pool;
