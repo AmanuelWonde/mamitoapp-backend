c47ddce3972f8072d96e629e90c2e35d72b86ae4
AbelMaireg <abelrighthere@gmail.com>
2024-07-24 15:18:14 +0300

    admin dashboard

---
+++ Controllers/admin/dashboard.js
@@ -0,0 +1,38 @@
+const db = require('../../Config/config');
+
+const controller = (req, res) => {
+
+  const sql = `
+SELECT 
+	DISTINCT win.name as "collectionName", win.created_at as "createdAt",
+	CASE
+		WHEN ans.id IS NULL THEN "not answered"
+        ELSE "answered"
+	END AS status,
+    (SELECT COUNT(DISTINCT ans2.user_username) FROM mamitogw_mamito.answers ans2 WHERE ans2.window_id = ans.window_id) AS "users that answered",
+    (SELECT COUNT(DISTINCT ans2.user_username) 
+     FROM mamitogw_mamito.answers ans2 
+     JOIN mamitogw_mamito.user usr2 ON ans2.user_username = usr2.username 
+     WHERE ans2.window_id = ans.window_id AND usr2.gender = 'M') AS "male users",
+    (SELECT COUNT(DISTINCT ans2.user_username) 
+     FROM mamitogw_mamito.answers ans2 
+     JOIN mamitogw_mamito.user usr2 ON ans2.user_username = usr2.username 
+     WHERE ans2.window_id = ans.window_id AND usr2.gender = 'F') AS "female users"
+FROM mamitogw_mamito.windows win
+LEFT JOIN mamitogw_mamito.answers ans
+	ON win.id = ans.window_id
+LEFT JOIN mamitogw_mamito.user usr
+	ON ans.user_username = usr.username;
+`
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: "error while processing request" });
+    }
+
+    return res.status(200).json({ success: true, data: result });
+  })
+}
+
+module.exports = controller;
