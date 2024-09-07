27a61d01faab4cf51703a656937ebf9adaadba8c
Amanuel Wonde <amanuelwt@gmail.com>
2024-09-07 10:30:42 +0300

    change

--- Middleware/user/forgotpassword.js
+++ Middleware/user/forgotpassword.js
@@ -1,109 +1,159 @@
 // module imports and configs
-const bcrypt = require('bcryptjs');
-const JWT = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const JWT = require("jsonwebtoken");
 
 // mysql configurations
-const db = require('../../Config/config');
+const db = require("../../Config/config");
 
 // model imports
-const { forgotPassword } = require('../../Models/User');
-const { status, responseInstance } = require('../../Models/response');
-const documentation = require('../../documentation/statusCodeDocumentation.json');
+const { forgotPassword } = require("../../Models/User");
+const { status, responseInstance } = require("../../Models/response");
+const documentation = require("../../documentation/statusCodeDocumentation.json");
 
 module.exports = (req, res) => {
+  const debug = require("debug")("user:chpd");
 
-    const debug = require('debug')('user:chpd');
+  let p1 = new Promise((resolve, reject) => {
+    let { error } = forgotPassword.validate(req.body);
+    if (error)
+      reject(
+        new responseInstance(
+          new status(6001, documentation[6001]),
+          error.details[0].message
+        )
+      );
+    else resolve(req.body);
+  });
 
-    let p1 = new Promise((resolve, reject) => {
-        let { error } = forgotPassword.validate(req.body);
-        if (error) reject(new responseInstance(new status(6001, documentation[6001]), error.details[0].message));
-        else resolve(req.body);
+  //   let checkAnswers = (body) => {
+  //     return new Promise((resolve, reject) => {
+  //       db.getConnection((error, connection) => {
+  //         if (error) {
+  //           debug(`Error: ${error}`);
+  //           reject(
+  //             new responseInstance(
+  //               new status(7001, documentation[7001]),
+  //               "this is backend issue"
+  //             )
+  //           );
+  //         }
+
+  //         const sql = "CALL ValidateRecoveryAnswers(?,?,?,?,?,?,?)";
+  //         const values = [
+  //           body.username,
+  //           body.id1,
+  //           body.id2,
+  //           body.id3,
+  //           body.ans1,
+  //           body.ans2,
+  //           body.ans3,
+  //         ];
+
+  //         connection.query(sql, values, (error, result) => {
+  //           if (error) {
+  //             debug(error);
+  //           } else {
+  //             // console.log(result)
+  //             if (result[0][0].status == 1071) {
+  //               reject(
+  //                 new responseInstance(
+  //                   new status(1071),
+  //                   "give the correct answer to the questions"
+  //                 )
+  //               );
+  //             } else if (result[0][0].status == 1070) {
+  //               resolve(body);
+  //             }
+  //           }
+  //         });
+  //       });
+  //     });
+  //   };
+
+  const passwordEncryption = (body) => {
+    return new Promise(async (resolve, reject) => {
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+          debug(err);
+          reject(
+            new responseInstance(
+              new status(6010, documentation[6010]),
+              "this is a backend issue"
+            )
+          );
+        }
+        bcrypt.hash(body.newpassword, salt, (err, result) => {
+          if (err) {
+            debug(err);
+            reject(
+              new responseInstance(
+                new status(6011, documentation[6011]),
+                "this is a backend issue"
+              )
+            );
+          }
+          resolve(result);
+        });
+      });
     });
+  };
 
-    let checkAnswers = (body) => {
-        return new Promise((resolve, reject) => {
-            db.getConnection((error, connection) => {
-                if (error) {
-                    debug(`Error: ${error}`);
-                    reject(new responseInstance(new status(7001, documentation[7001]), 'this is backend issue'));
-                }
+  const store = (encrypted) => {
+    return new Promise((resolve, reject) => {
+      db.getConnection((error, connection) => {
+        if (error) {
+          debug(`Error: ${error}`);
+          reject(
+            new responseInstance(
+              new status(7001, documentation[7001]),
+              "this is backend issue"
+            )
+          );
+        }
 
-                const sql = 'CALL ValidateRecoveryAnswers(?,?,?,?,?,?,?)';
-                const values = [body.username, body.id1, body.id2, body.id3, body.ans1, body.ans2, body.ans3];
+        const sql = "CALL UpdatePassword(?, ?)";
+        const values = [req.body.username, encrypted];
 
-                connection.query(sql, values, (error, result) => {
-                    if (error) {
-                        debug(error);
-                    } else {
-                        // console.log(result)
-                        if (result[0][0].status == 1071) {
-                            reject(new responseInstance(new status(1071), 'give the correct answer to the questions'));
-                        } else if (result[0][0].status == 1070) {
-                            resolve(body);
-                        }
-                    }
-                })
-            })
-        })
-    }
+        connection.query(sql, values, (error, result) => {
+          if (error) {
+            debug(error);
+            reject(
+              new responseInstance(
+                new status(7003, documentation[7003]),
+                "this is a backend issue"
+              )
+            );
+          } else {
+            if (result[0][0].status == 1061) {
+              debug(result);
+              reject(
+                new responseInstance(
+                  new status(1061, documentation[1061]),
+                  "user not registered"
+                )
+              );
+            } else {
+              resolve(result[0][0]);
+            }
+          }
+        });
+      });
+    });
+  };
 
-    const passwordEncryption = (body) => {
-        return new Promise(async (resolve, reject) => {
-            bcrypt.genSalt(10, (err, salt) => {
-                if (err) {
-                    debug(err);
-                    reject(new responseInstance(new status(6010, documentation[6010]), 'this is a backend issue'));
-                };
-                bcrypt.hash(body.newpassword, salt, (err, result) => {
-                    if (err) {
-                        debug(err);
-                        reject(new responseInstance(new status(6011, documentation[6011]), 'this is a backend issue'));
-                    }
-                    resolve(result);
-                })
-            })
-        })
-    }
+  const sender = (result) => {
+    res.send(
+      new responseInstance(
+        new status(result.status),
+        "password changed successfully"
+      )
+    );
+  };
 
-    const store = (encrypted) => {
-        return new Promise((resolve, reject) => {
-
-            db.getConnection((error, connection) => {
-                if (error) {
-                    debug(`Error: ${error}`);
-                    reject(new responseInstance(new status(7001, documentation[7001]), 'this is backend issue'));
-                }
-
-                const sql = 'CALL UpdatePassword(?, ?)';
-                const values = [req.body.username, encrypted];
-
-                connection.query(sql, values, (error, result) => {
-                    if (error) {
-                        debug(error);
-                        reject(new responseInstance(new status(7003, documentation[7003]), 'this is a backend issue'));
-                    } else {
-                        if (result[0][0].status == 1061) {
-                            debug(result);
-                            reject(new responseInstance(new status(1061, documentation[1061]), 'user not registered'));
-                        } else {
-                            resolve(result[0][0])
-                        }
-                    }
-                })
-            })
-
-        })
-    }
-
-    const sender = (result) => {
-        res.send(new responseInstance(new status(result.status), 'password changed successfully'))
-    }
-
-    p1
-        .then((body) => checkAnswers(body))
-        .then((body) => passwordEncryption(body))
-        .then((encrypted) => store(encrypted))
-        .then((result) => sender(result))
-        .catch((error) => res.send(error))
-
-}
+  p1
+    // .then((body) => checkAnswers(body))
+    .then((body) => passwordEncryption(body))
+    .then((encrypted) => store(encrypted))
+    .then((result) => sender(result))
+    .catch((error) => res.send(error));
+};
--- Models/User.js
+++ Models/User.js
@@ -4,18 +4,24 @@ const userSchema = Joi.object({
   username: Joi.string().min(6).required(),
   name: Joi.string().required(),
   gender: Joi.string().length(1).required(),
-  birthdate: Joi.string().length(10).pattern(/^\d{4}-\d{2}-\d{2}$/).required(),
+  birthdate: Joi.string()
+    .length(10)
+    .pattern(/^\d{4}-\d{2}-\d{2}$/)
+    .required(),
   password: Joi.string().min(8).required(),
   phone: Joi.string().pattern(/^251[7,9]\d{8}$/),
   bio: Joi.string().allow(null).required(),
   religion: Joi.number().integer().positive().required(),
   changeOneSelf: Joi.number().integer().positive().required(),
   latitude: Joi.number().required(),
-  longitude: Joi.number().required()
+  longitude: Joi.number().required(),
 });
 
 const loggerSchema = Joi.object({
-  username: Joi.alt(Joi.string().pattern(/^251[7,9]\d{8}$/), Joi.string().min(6).required()),
+  username: Joi.alt(
+    Joi.string().pattern(/^251[7,9]\d{8}$/),
+    Joi.string().min(6).required()
+  ),
   password: Joi.string().min(8).required(),
 });
 
@@ -23,13 +29,15 @@ const userUpdateSchema = Joi.object({
   username: Joi.string().min(6).required(),
   name: Joi.string(),
   gender: Joi.string().length(1),
-  birthdate: Joi.string().length(10).pattern(/^\d{4}-\d{2}-\d{2}$/),
+  birthdate: Joi.string()
+    .length(10)
+    .pattern(/^\d{4}-\d{2}-\d{2}$/),
   phone: Joi.string().pattern(/^251[7,9]\d{8}$/),
   bio: Joi.string().allow(null),
   religion: Joi.number().integer().positive(),
   changeOneSelf: Joi.number().integer().positive(),
   latitude: Joi.number(),
-  longitude: Joi.number()
+  longitude: Joi.number(),
 });
 
 const questionFillSchema = Joi.object({
@@ -40,33 +48,41 @@ const questionFillSchema = Joi.object({
   ans1: Joi.string().required(),
   ans2: Joi.string().required(),
   ans3: Joi.string().required(),
-})
+});
 
 const changePassword = Joi.object({
   username: Joi.string().required(),
   oldpassword: Joi.string().required(),
-  newpassword: Joi.string().required()
-})
+  newpassword: Joi.string().required(),
+});
 
 const forgotPassword = Joi.object({
   username: Joi.string().required(),
-  id1: Joi.number().required(),
-  id2: Joi.number().required(),
-  id3: Joi.number().required(),
-  ans1: Joi.string().required(),
-  ans2: Joi.string().required(),
-  ans3: Joi.string().required(),
-  newpassword: Joi.string().required()
-
-})
+  // id1: Joi.number().required(),
+  // id2: Joi.number().required(),
+  // id3: Joi.number().required(),
+  // ans1: Joi.string().required(),
+  // ans2: Joi.string().required(),
+  // ans3: Joi.string().required(),
+  newpassword: Joi.string().required(),
+});
 
 const getRcvQst = Joi.object({
-  username: Joi.string().required()
-})
+  username: Joi.string().required(),
+});
 // adf
 const updateVerification = Joi.object({
   username: Joi.string().required(),
   verfication: Joi.boolean().required(),
-})
+});
 
-module.exports = { userSchema, loggerSchema, userUpdateSchema, questionFillSchema, changePassword, forgotPassword, getRcvQst, updateVerification };
+module.exports = {
+  userSchema,
+  loggerSchema,
+  userUpdateSchema,
+  questionFillSchema,
+  changePassword,
+  forgotPassword,
+  getRcvQst,
+  updateVerification,
+};
